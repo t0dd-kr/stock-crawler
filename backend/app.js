cf3fefb7cc028756a914c7229e35d975fd0663e9
t0dd-kr <swjasontodd@gmail.com>
2018-03-21 20:51:26 +0900

    api2를 api로 통합

--- backend/app.js
+++ backend/app.js
@@ -36,7 +36,6 @@ app.use(passport.session()); // 세션 연결
 
 app.use('/', index);
 app.use('/api', api);
-app.use('/api2', api2);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- backend/routes/api.js
+++ backend/routes/api.js
@@ -114,4 +114,21 @@ router.get('/info/:id', (req, res, next) => {
   })
 });
 
+router.get('/test', (req, res, next) => {
+  var url = "https://dobset.io/";
+  request(url, function(error, response, body) {
+    if (error) throw error;
+
+    var $ = cheerio.load(body);
+
+    var postElements = $("section.posts article.post");
+    postElements.each(function() {
+      var postTitle = $(this).find("h1").text();
+      var postUrl = $(this).find("h1 a").attr("href");
+    });
+    res.send({});
+  });
+});
+
+
 module.exports = router;
